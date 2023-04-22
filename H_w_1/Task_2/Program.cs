60be39c94160f6dac7cbd22c11d0c75338f88d58
*Elizarov <Super.elizarov1451@mail.ru>
2023-04-22 22:50:22 +0300

    Решение 2-х задач

---
+++ H_w_1/Task_2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и 
+// выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Clear();
+
+int Num1, Num2, Num3;
+
+Console.WriteLine("Введите 1-е число");
+Num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите 2-е число");
+Num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите 3-е число");
+Num3 = Convert.ToInt32(Console.ReadLine());
+
+if (Num1 > Num2 && Num1 > Num3)
+{
+  Console.WriteLine($"Max={Num1}");
+}
+
+else if (Num2 > Num1 && Num2 > Num3)
+{
+  Console.WriteLine($"Max={Num2}");
+}
+
+else if (Num3 > Num1 && Num3 > Num2)
+{
+  Console.WriteLine($"Max={Num3}");
+}
+
+else if (Num1 == Num2 && Num2 == Num3)
+{
+  Console.WriteLine("Числа равны друг другу");
+}
