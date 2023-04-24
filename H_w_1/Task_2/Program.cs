2fcf5df4a03878146a035f8ce0d017131e482522
Erell-X <86251878+Erell-X@users.noreply.github.com>
2023-04-24 09:19:18 +0300

    Программа к задаче №4

--- H_w_1/Task_2/Program.cs
+++ H_w_1/Task_2/Program.cs
@@ -6,34 +6,49 @@
 // 22 3 9 -> 22
 
 Console.Clear();
-
+  
 int Num1, Num2, Num3;
 
-Console.WriteLine("Введите 1-е число");
+Console.WriteLine("Введите 1-е число (Num1)");
 Num1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите 2-е число");
+Console.WriteLine("Введите 2-е число (Num2)");
 Num2 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите 3-е число");
+Console.WriteLine("Введите 3-е число (Num3)");
 Num3 = Convert.ToInt32(Console.ReadLine());
 
 if (Num1 > Num2 && Num1 > Num3)
 {
-  Console.WriteLine($"Max={Num1}");
+  Console.WriteLine($"Max = Num1 = {Num1}");
 }
 
 else if (Num2 > Num1 && Num2 > Num3)
 {
-  Console.WriteLine($"Max={Num2}");
+  Console.WriteLine($"Max = Num2 = {Num2}");
 }
 
 else if (Num3 > Num1 && Num3 > Num2)
 {
-  Console.WriteLine($"Max={Num3}");
+  Console.WriteLine($"Max = Num3 = {Num3}");
+}
+
+else if (Num1 == Num2 && Num1 > Num3)
+{
+  Console.WriteLine($"Max = Num1 = Num2 = {Num1}");
+}
+
+else if (Num2 == Num3 && Num2 > Num1)
+{
+  Console.WriteLine($"Max = Num2 = Num3 = {Num2}");
+}
+
+else if (Num1 == Num3 && Num1 > Num2)
+{
+  Console.WriteLine($"Max = Num1 = Num3 = {Num1}");
 }
 
 else if (Num1 == Num2 && Num2 == Num3)
 {
-  Console.WriteLine("Числа равны друг другу");
+  Console.WriteLine($"Числа равны друг другу, Max = Num1 = Num2 = Num3 = {Num1}");
 }
