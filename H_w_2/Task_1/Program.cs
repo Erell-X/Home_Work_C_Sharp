746c853fbf46bff96fb933354370d5eb5d8c5639
*Elizarov <Super.elizarov1451@mail.ru>
2023-05-28 10:23:48 +0300

    Написание программы к Задаче1 ДЗ-4 по C#

---
+++ H_w_2/Task_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.Clear();
+
+int A, B;
+double degree;
+
+System.Console.Write("Введите число A: ");
+A = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Введите число B: ");
+B = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine();
+
+degree = Math.Pow(A, B);
+System.Console.WriteLine($"{A}^{B}={degree}");
