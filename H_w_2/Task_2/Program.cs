2e6163a430488d1b9589dc7373a7452e86f47d65
*Elizarov <Super.elizarov1451@mail.ru>
2023-05-28 13:31:37 +0300

    Программа к задаче2 ДЗ4 по c#

---
+++ H_w_2/Task_2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и 
+// выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.Clear();
+
+System.Console.Write("Введите число: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+
+int Number = Num;
+int NewNum = 0;
+int Sum = 0;
+
+while (Num > 9)
+{
+  NewNum = Num % 10;
+  Sum = Sum + NewNum;
+  Num = Num / 10;
+}
+
+Sum = Sum + Num;
+System.Console.Write($"Сумма цифр числа {Number} равна {Sum}");
---
+++ H_w_2/Task_3/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+ 
+namespace Array_from_set_user_parameter5
+{
+/* Продемонстрировать одномерный массив из дробных чисел.
+   Количество элементов массива задаётся с клавиатуры */
+  class Program
+   {
+    static void Main(string[] args)
+     {
+     // Задать с клавиатуры число элементов массива
+        int n;
+        Console.WriteLine("Введите число элементов массива");
+        n = Convert.ToInt16(Console.ReadLine());
+     // Объявим одномерный массив Data с n-элементами 
+        double [] Data = new double[n];
+        int i = 0;
+       while (i < n)
+        {
+           Console.WriteLine("Введите элемент массива");
+        /* Вводим элементы массива с клавиатуры 
+         * и заполняем ими массив */
+           Data[i] = double.Parse(Console.ReadLine());
+           Console.WriteLine();
+           i++;
+        }
+   // Вывод содержимого массива
+     for (i = 0; i < n; i = i + 1)
+     Console.WriteLine("Элемент[" + i + "]: " + Data[i]);
+     Console.ReadKey();
+   }
+ }
+}
