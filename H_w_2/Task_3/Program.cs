deae55041dbe768b379fa43cd0f1893c1fda3d6b
*Elizarov <Super.elizarov1451@mail.ru>
2023-05-29 18:54:38 +0300

    Программа к задаче 3 ДЗ-4 по C#

--- H_w_2/Task_3/Program.cs
+++ H_w_2/Task_3/Program.cs
@@ -5,24 +5,11 @@
 
 // 6, 1, 33 -> [6, 1, 33]
 
-// int[] nums = new int[8];
-// int mas = 0;
-
-// mas = new Random();
-// nums[0] = new Random();
-// nums[1] = 10;
-// nums[2] = 100;
-// nums[3] = 18;
-// nums[4] = 78;
-// nums[5] = 23;
-// nums[6] = 63;
-// nums[7] = 9;
-// nums[8] = 87;
-// nums[9] = 49;
-
-int[] array = new int[8];
-Random Rand = new Random();
-for (int x = 0; x < 8; x++)
-{
-     array[x] = Rand.Next(1, 9);
-}
+int[] MyArray = new int[8]; // объявление массива с именем MyArray типа int, содержащего 8 элементов (выделение памяти)
+    Random rand = new Random(); // объявление метода
+   
+    for (int i = 0; i < MyArray.Length; i++) // работа цикла for
+    {
+      MyArray[i] = rand.Next(1000); // максимально возможное значение любого элемента массива не может быть больше числа 1000
+      Console.Write($"{MyArray[i]}, ");
+    }
