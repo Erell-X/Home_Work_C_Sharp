0475594664b537e677ef52e0ec6deb4e896a1c29
*Elizarov <Super.elizarov1451@mail.ru>
2023-05-31 19:15:31 +0300

    1-я Попытка написать Программу к Задаче-1 ДЗ-5

---
+++ H_w_3/Task_1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными 
+// трёхзначными числами. Напишите программу, которая покажет количество
+// чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] Array(int size)
+{
+  int[] arr = new int[size];
+  Random rnd = new Random();
+
+  for (int i = 0; i < arr.Length; i++)
+  {
+    arr[i] = rnd.Next(100, 1000);
+  }
+  return arr;
+}
+
+System.Console.Write("Введите размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = Array(length);
+System.Console.WriteLine(string.Join(", ", array));
+
+int g = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+  if (array[i] % 2 = 0)
+  {
+    g += 1;
+  }
+
+}
+
+System.Console.WriteLine(g);
