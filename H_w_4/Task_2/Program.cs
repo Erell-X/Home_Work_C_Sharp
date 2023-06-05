e04a5916f3771103cccd7f7b1050eab3beb94d48
*Elizarov <Super.elizarov1451@mail.ru>
2023-06-05 22:24:15 +0300

    1-я версия Программы к Задаче-2 ДЗ-6

--- H_w_4/Task_2/Program.cs
+++ H_w_4/Task_2/Program.cs
@@ -3,3 +3,44 @@
 // значения b1, k1, b2 и k2 задаются пользователем.
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double[] Array (int size)
+{
+  double[] array = new double[size];
+
+  for (int i = 0; i < array.Length; i++)
+  {
+    switch(size)
+    {
+      case 1:
+      {
+          Console.WriteLine("Введите b1");
+          int b1 = Convert.ToInt32(Console.ReadLine());
+          Console.WriteLine();
+          break;
+      }
+      case 2:
+      {
+          Console.WriteLine("Введите k1");
+          int b1 = Convert.ToInt32(Console.ReadLine());
+          Console.WriteLine();
+          break;
+      }
+      case 3:
+      {
+          Console.WriteLine("Введите b2");
+          int b1 = Convert.ToInt32(Console.ReadLine());
+          Console.WriteLine();
+          break;
+      }
+      case 4:
+      {
+          Console.WriteLine("Введите k2");
+          int b1 = Convert.ToInt32(Console.ReadLine());
+          Console.WriteLine();
+          break;
+      }
+    }
+  return array;
+  }
+}
