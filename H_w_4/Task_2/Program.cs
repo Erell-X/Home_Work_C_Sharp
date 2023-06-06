87d53d66ae253363b1fac0c4a88a73ebc92d0576
*Elizarov <Super.elizarov1451@mail.ru>
2023-06-06 22:26:44 +0300

    Программа к Задаче-2 ДЗ-6 по c#

--- H_w_4/Task_2/Program.cs
+++ H_w_4/Task_2/Program.cs
@@ -4,43 +4,73 @@
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-double[] Array (int size)
+
+int[] Array(int size)
 {
-  double[] array = new double[size];
+  int[] array = new int[size];
 
   for (int i = 0; i < array.Length; i++)
   {
-    switch(size)
+    switch (i)
     {
+      case 0:
+        {
+          Console.WriteLine("Введите коэффициент k1");
+          int k1 = Convert.ToInt32(Console.ReadLine());
+          array[i] = k1;
+          break;
+        }
       case 1:
-      {
-          Console.WriteLine("Введите b1");
+        {
+          Console.WriteLine("Введите коэффициент b1");
           int b1 = Convert.ToInt32(Console.ReadLine());
-          Console.WriteLine();
+          array[i] = b1;
           break;
-      }
+        }
       case 2:
-      {
-          Console.WriteLine("Введите k1");
-          int b1 = Convert.ToInt32(Console.ReadLine());
-          Console.WriteLine();
+        {
+          Console.WriteLine("Введите коэффициент k2");
+          int k2 = Convert.ToInt32(Console.ReadLine());
+          array[i] = k2;
           break;
-      }
+        }
       case 3:
-      {
-          Console.WriteLine("Введите b2");
-          int b1 = Convert.ToInt32(Console.ReadLine());
-          Console.WriteLine();
+        {
+          Console.WriteLine("Введите коэффициент b2");
+          int b2 = Convert.ToInt32(Console.ReadLine());
+          array[i] = b2;
           break;
-      }
-      case 4:
-      {
-          Console.WriteLine("Введите k2");
-          int b1 = Convert.ToInt32(Console.ReadLine());
-          Console.WriteLine();
-          break;
-      }
+        }
+
     }
-  return array;
   }
+  return array;
 }
+
+int size = 4;
+int[] array = Array(size);
+// Console.WriteLine(string.Join(", ", array));
+
+double x = 0,
+       y = 0;
+
+int k1 = 0,
+    b1 = 0,
+    k2 = 0,
+    b2 = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+  if (i == 0) k1 = array[i];
+  if (i == 1) b1 = array[i];
+  if (i == 2) k2 = array[i];
+  if (i == 3) b2 = array[i];
+}
+
+x = b2 - b1;
+y = k1 - k2;
+
+x = (x) / (y);
+y = k1 * x + b1;
+
+Console.WriteLine($"Координаты точки пересечения двух прямых: ({x}; {y})");
