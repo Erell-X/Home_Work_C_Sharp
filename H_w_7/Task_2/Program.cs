b86789d9bd11588fb31c5fcd6a1bddb73e094f6a
*Elizarov <Super.elizarov1451@mail.ru>
2023-06-18 08:18:29 +0300

    Программа к Задаче-2 ДЗ-9 по C#

---
+++ H_w_7/Task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int sumNumbers(int M, int N)
+{
+  if (M <= N) return sumNumbers(M + 1, N) + M;
+  else return 0;
+}
+
+System.Console.WriteLine(sumNumbers(1, 15));
