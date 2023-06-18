2af7181174bb2f251abe8627ca939198e20614bf
*Elizarov <Super.elizarov1451@mail.ru>
2023-06-18 06:46:45 +0300

    Программа к Задаче-1 ДЗ-9

---
+++ H_w_7/Task_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+string seriesOfNumbers(int i, int N)
+{
+  if (i <= N) return seriesOfNumbers(i+1, N) + $"{i} ";
+  else return String.Empty;
+}
+System.Console.WriteLine(seriesOfNumbers(1, 10));
